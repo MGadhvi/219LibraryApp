0c9fc813cda8d3ebe8d70f5aa4aa3a02d5ec97be
Manish Gadhvi <mgadhvi@spartaglobal.com>
2023-05-23 12:16:09 +0100

    added git ignore

---
+++ src/main/java/com/sparta/mg/libraryapplication/model/AuthorRepository.java
@@ -0,0 +1,4 @@
+package com.sparta.mg.libraryapplication.model;
+
+public interface AuthorRepository extends org.springframework.data.jpa.repository.JpaRepository<com.sparta.mg.libraryapplication.model.AuthorDTO, java.lang.Integer> {
+}
